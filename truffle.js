67692234cdc40dfb6fca61998932c7ca8a443c22
Ruofan Wang <1009924028@qq.com>
2021-04-20 10:09:59 -0500

    inital

--- truffle.js
+++ truffle.js
@@ -57,7 +57,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545, // This is the conventional port. If you're using the Ganache Blockchain, change port value to the Ganache default port 7545. If you're using Truffle develop network, change port value to 9545
-      network_id: "666", // Match any network id. You may need to replace * with your network Id
+      network_id: "*", // Match any network id. You may need to replace * with your network Id
       from: "", // Add your unlocked account within the double quotes
       gas: 4444444
     },
