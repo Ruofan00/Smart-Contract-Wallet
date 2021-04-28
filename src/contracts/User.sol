09c507a23693662849376fe3753819da0f5d2d7c
Ruofan Wang <1009924028@qq.com>
2021-04-27 20:09:38 -0500

    edit user.sol

--- src/contracts/User.sol
+++ src/contracts/User.sol
@@ -1,29 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 pragma experimental ABIEncoderV2;
-import "./Messenger.sol";
 
 contract User {
- //   address[] public owners;
     address public cur_owner;
-   // address contract;
     mapping(address=>bool) public isOwner;
     mapping(address => string[]) messages;
     uint cur_approval;
     mapping(address => uint256) balances;
     mapping(address=>string) users;
     uint public numOfApprov;
-    RecoverTools tool;
+    //RecoverTools tool;
     
-    // struct Transaction {
-    //     address to;
-    //     uint value;
-    //     bytes data;
-    //     bool executed;
-    //     uint numOfApprov;
-    // }
+    event Debit(address indexed_from, address indexed_to, uint _value);
     
-    // Transaction[] public transcations;
     
     modifier onlyOwner() {
         require(isOwner[msg.sender],"not owner");
@@ -31,47 +21,20 @@ contract User {
     }
 
 
-    
     constructor(address _cur_owners, uint _numOfApprov) {
-        // require(_owners.length>0,'need some users');
-        // require(
-        //     _numOfApprov>0 && _numOfApprov<=_owners.length,
-        //     'need valid number of approval'
-        // );
-        // for(uint i=0;i<_owners.length;i++) {
-        //     address owner = _owners[i];
-            
-        //     require(owner!=address(0),'invalid owner');
-        //     require(!isOwner[owner],'owner not unique');
-        //     //tool=new RecoverTools();
-        //     isOwner[owner] = true;
-        //     cur_owner = payable(msg.sender);
-        //     owners.push(owner);
-            
-        // }
         cur_owner = _cur_owners;
         numOfApprov = _numOfApprov;
     }
     
-    // function getOwners() public view returns (address[] memory) {
-    //     return owners;
-    // }
     
     function getMessages() public view returns(string[] memory) {
         return messages[address(this)];
     }
 
     function getOwner() public view returns(address) {
-    	return cur_owner;
+        return cur_owner;
     }
     
-    // function SignUp() public {
-        
-    // }
-    
-    // function SignIn(address addr, string memory username) public {
-        
-    // }
     
     function sendMessage(address _recipient, string calldata _message) public {
         messages[_recipient].push(_message);
@@ -94,6 +57,15 @@ contract User {
     function deposit() public payable {
         
     }
+    
+    receive() external payable{}
+    
+    function debit(address addr, uint value) external {
+        emit Debit(msg.sender,addr,value);
+         address payable temp = payable(addr);
+         temp.transfer(value);
+         
+     }
     
     function withdraw() public {
         uint amount = address(this).balance;
@@ -125,21 +97,11 @@ contract User {
     function compareTo(string memory a, string memory b) public pure returns(bool) {
         return keccak256(bytes(a)) == keccak256(bytes(b));
     }
-    event Transfer(address indexed_from, address indexed_to, uint _value);
 
     function transfer(address payable _to, uint _amount) public {
-        emit Transfer(msg.sender,_to,_amount);
         (bool success,) = _to.call{value:_amount}("");
         require(success,"Failed to send either");
     }
     
     
 }
-
-// contract RecoverTools {
-    
-//     function recover(address[] memory owners, uint numOfApprov) public returns(Messenger) {
-//         Messenger n_contract = new Messenger(owners,numOfApprov);
-//         return n_contract;
-//     }
-// }
